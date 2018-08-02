6f71320eb06ad115da0e7dbf3214be5a52fa835b
baklavacado <benny.l.wu@gmail.com>
2018-08-02 02:43:14 -0700

    init w/all renders

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 2332;
+
+app.use(morgan('dev'));
+app.use('/songs/:id', express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }, 
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+};
